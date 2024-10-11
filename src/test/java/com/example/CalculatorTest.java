c6d605eeda6f620f7371c9a074cdffe0f90f3825
unknown <g.tanzharikova@gmail.com>
2024-10-11 13:39:16 +0200

    Add more tests

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,57 +1,75 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     void canAdd() {
         //given
         Calculator calc = new Calculator();
 
         //when
-        int result = calc.add(3, 5);
+        int result = calc.add(2, 5);
+        int result2 = calc.add(-3, 5);
+        int result3 = calc.add(4, -5);
 
         //then
-        assertEquals(8, result);
+        assertAll(() -> assertEquals(7, result),
+                () -> assertEquals(2, result2),
+                () -> assertEquals(-1, result3)
+        );
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void canSub() {
         //given
         Calculator calc = new Calculator();
 
         //when
-        int result = calc.subtract(4, 5);
+        int result = calc.subtract(2, 5);
+        int result2 = calc.subtract(-3, 5);
+        int result3 = calc.subtract(4, -5);
 
         //then
-        assertEquals(-1, result);
+        assertAll(() -> assertEquals(-3, result),
+                () -> assertEquals(-8, result2),
+                () -> assertEquals(9, result3)
+        );
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void canMult() {
         //given
         Calculator calc = new Calculator();
 
         //when
-        int result = calc.multiply(4, 5);
+        int result = calc.multiply(2, 5);
+        int result2 = calc.multiply(-3, 5);
+        int result3 = calc.multiply(4, -5);
 
         //then
-        assertEquals(20, result);
+        assertAll(() -> assertEquals(10, result),
+                () -> assertEquals(-15, result2),
+                () -> assertEquals(-20, result3)
+        );
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void canDiv() {
         //given
         Calculator calc = new Calculator();
 
         //when
         int result = calc.divide(10, 5);
+        int result2 = calc.divide(-5, 5);
+        int result3 = calc.divide(-20, -5);
 
         //then
-        assertEquals(2, result);
+        assertAll(() -> assertEquals(2, result),
+                () -> assertEquals(-1, result2),
+                () -> assertEquals(4, result3)
+        );
     }
-
-
 }
